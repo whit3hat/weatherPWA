86723dcd27780b1e96d96be2fc6aa9bef7e0152c
Spencer Wood <spencer.wood@me.com>
2020-08-04 20:51:35 -0500

    added the city card below the search input box.

--- src/App.js
+++ src/App.js
@@ -20,6 +20,14 @@ const App = () => {
   return (
     <div className='main-container'>
       <input type='text' className='search' placeholder='Search...' value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+      {weather.main && (
+        <div className='city'>
+          <h2 className='city-name'>
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
